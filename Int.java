d7e532ea7d3c098c31d8d0dcea14c2645591dbf0
Konsei0516 <masaya19960516@gmail.com>
2020-06-16 18:44:56 +0900

    変数の代入と再代入
    再代入時は型宣言を必要としない

---
+++ Int.java
@@ -0,0 +1,8 @@
+public class Int {
+  public static void main(String[] args) {
+    int age = 20;
+    System.out.println("私の年齢は"+ age);
+    age = 31;
+    System.out.println("私の年齢は"+ age);
+  }
+}
