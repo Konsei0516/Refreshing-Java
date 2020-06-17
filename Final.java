180901f1f8c1df6017ff9f260d483d62e360b0f8
Konsei0516 <masaya19960516@gmail.com>
2020-06-17 12:56:15 +0900

    finalの使用法
    finalを使用すると定数化できる

---
+++ Final.java
@@ -0,0 +1,11 @@
+public class Final {
+  public static void main(String[] args) {
+    final double pi = 3.14;
+    int pie = 5;
+    System.out.println("半径"+ pie + "cmのパイの面積");
+    System.out.println(pie * pie * pi);
+    pie = 10;
+    System.out.println("半径"+ pie + "cmのパイの面積");
+    System.out.println(pie * pie * pi);
+  }
+}
