3220186a04384706d0ac089446142dff7be23bf1
Konsei0516 <masaya19960516@gmail.com>
2020-06-19 20:35:58 +0900

    強制的な型変換
    大きい方から小さい型へ強制的に変換する。

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    float f = 3;
-    double d = f;
-    System.out.println(f);
-    System.out.println(d);
+    int age = (int)3.2;
+    System.out.println(age);
   }
 }
