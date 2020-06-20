3a8ce01e6bfdb4f0d7bfee2c8dcf8664d75c3a20
Konsei0516 <masaya19960516@gmail.com>
2020-06-20 21:24:17 +0900

    命令実行文

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int age = (int)3.2;
-    System.out.println(age);
+    String name = "すがわら";
+    String message;
+    message = name + "さん,こんにちは";
+    System.out.println(message);
   }
 }
