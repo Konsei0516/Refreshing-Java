7b32852ea0adf998d472b9dc199246246f54e864
Konsei0516 <masaya19960516@gmail.com>
2020-06-23 22:21:49 +0900

    while文でのループ処理

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    boolean tenki = true;
-    if (tenki == true) {
-      System.out.println("洗濯します");
-    } else {
-      System.out.println("DVDを見ます");
+    boolean doorClose = true;
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
     }
   }
 }
