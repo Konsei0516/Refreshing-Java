fc51380c1ff27fdd1925a38bc7edac34ff1c0ecc
Konsei0516 <masaya19960516@gmail.com>
2020-06-24 18:46:16 +0900

    switch文

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    boolean doorClose = true;
-    while (doorClose == true) {
-      System.out.println("ノックする");
-      System.out.println("1分待つ");
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random().nextInt(5) + 1;
+    switch (fortune) {
+      case 1:
+      case 2:
+        System.out.println("いいね");
+        break;
+      case 3:
+        System.out.println("普通です");
+        break;
+      case 4:
+      case 5:
+        System.out.println("うーん");
     }
   }
 }
