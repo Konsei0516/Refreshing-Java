cad36bfc9046ef2484c62a53a0c87fd37fbc3976
Konsei0516 <masaya19960516@gmail.com>
2020-06-25 23:41:12 +0900

    for文

--- Main.java
+++ Main.java
@@ -1,18 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("あなたの運勢を占います");
-    int fortune = new java.util.Random().nextInt(5) + 1;
-    switch (fortune) {
-      case 1:
-      case 2:
-        System.out.println("いいね");
-        break;
-      case 3:
-        System.out.println("普通です");
-        break;
-      case 4:
-      case 5:
-        System.out.println("うーん");
+    for (int i = 0; i < 3; i ++ ) {
+      System.out.println("現在" + (i + 1) + "週目");
     }
   }
 }
