ab1f9b7d469bd972bc9c3da0a8afaa83cc289118
Konsei0516 <masaya19960516@gmail.com>
2020-06-27 23:26:36 +0900

    配列の作成
    配列の作成と配列の中身の確認

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    for (int i = 1; i < 10; i ++ ) {
-      for (int j = 1; j < 10; j ++ ) {
-        System.out.println(i * j);
-        System.out.println(" ");
-      }
-      System.out.println("");
-    }
+    int[] scores = new int[5];
+    int num = scores.length;
+    System.out.println("要素の数:" + num);
   }
 }
