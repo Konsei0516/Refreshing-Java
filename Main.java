0e2630ef28d32f49e64a56c509f68e026430fde7
Konsei0516 <masaya19960516@gmail.com>
2020-06-28 23:35:16 +0900

    配列の代入

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     int[] scores = new int[5];
-    int num = scores.length;
-    System.out.println("要素の数:" + num);
+    scores[1] = 30;
+    System.out.println(scores[1]);
   }
 }
