ceb5db5aaae947f4c46a19bba53728936d829c98
Konsei0516 <masaya19960516@gmail.com>
2020-06-29 21:52:39 +0900

    配列の初期化

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     int[] scores = new int[5];
-    scores[1] = 30;
-    System.out.println(scores[1]);
+    System.out.println(scores[0]);
   }
 }
