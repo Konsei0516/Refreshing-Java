a97237cc55e37938b47408ba2bf3cea6fad9e0a7
Konsei0516 <masaya19960516@gmail.com>
2020-06-30 23:10:11 +0900

    for文で配列を使う

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int[] scores = new int[5];
-    System.out.println(scores[0]);
+    int[] scores = {20,30,40,50,80 };
+    for (int i = 0; i < scores.length; i ++ ){
+      System.out.println(scores[i]);
+    }
   }
 }
