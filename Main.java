801aa0c977006bfe8948ecd6ffd87134cb9817ed
Konsei0516 <masaya19960516@gmail.com>
2020-07-01 22:11:26 +0900

    拡張for文

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     int[] scores = {20,30,40,50,80 };
-    int count = 0;
-    for (int i = 0; i < scores.length; i ++ ){
-      if (scores[i] >= 50){
-        count ++ ;
-      }
+    for (int value : scores){
+      System.out.println(value);
     }
-    System.out.println("50点以上の科目の数は" + count);
+    
   }
 }
