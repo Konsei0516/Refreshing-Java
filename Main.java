1ee7c05a659188a1cb362a92f3f1ae7a5a650f1f
Konsei0516 <masaya19960516@gmail.com>
2020-07-03 22:37:12 +0900

    ガレージコレクション

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    int[] arrayA = {1,2,3};
-    int[] arrayB;
-    arrayB = arrayA;
-    arrayB[0] = 100;
-    System.out.println(arrayA[0]);
+    boolean judge = true;
+    if (judge == true){
+      int[] array = {1,2,3};
+    }
   }
 }
