47fd9f7d5d55371b3191e3e24eaea59316f7ff19
Konsei0516 <masaya19960516@gmail.com>
2020-07-04 21:12:58 +0900

    null利用のコード

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    boolean judge = true;
-    if (judge == true){
-      int[] array = {1,2,3};
-    }
+    int[] array = {1,2,3};
+    array = null;
+    array[0] = 10;
   }
 }
