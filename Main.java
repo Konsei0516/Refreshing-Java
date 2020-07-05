fb950f544c643da11d1e39edc76d58b617b4ec4f
Konsei0516 <masaya19960516@gmail.com>
2020-07-05 22:45:13 +0900

    二次元配列の利用

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    int[] array = {1,2,3};
-    array = null;
-    array[0] = 10;
+    int[][] scores = new int[2][3];
+    scores[0][0] = 40;
+    scores[0][1] = 50;
+    scores[0][2] = 60;
+    scores[1][0] = 80;
+    scores[1][1] = 60;
+    scores[1][2] = 70;
+    System.out.println(scores[1][1]);
   }
 }
