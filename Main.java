f911511a7014831ccd29eb353ad935a339e9cc59
Konsei0516 <masaya19960516@gmail.com>
2020-07-06 20:38:06 +0900

    親配列とこ配列の要素数表示

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    int[][] scores = new int[2][3];
-    scores[0][0] = 40;
-    scores[0][1] = 50;
-    scores[0][2] = 60;
-    scores[1][0] = 80;
-    scores[1][1] = 60;
-    scores[1][2] = 70;
-    System.out.println(scores[1][1]);
+    int[][] scores = {{40,50,60},{80,60,70}};
+    System.out.println(scores.length);
+    System.out.println(scores[0].length);
   }
 }
