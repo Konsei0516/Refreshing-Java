7bd5b5142b6ebb531ce2aa12501bf3260f5c02b4
Konsei0516 <masaya19960516@gmail.com>
2020-07-07 17:06:51 +0900

    戻り値

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
-  public static void main(String[] args){
-    add(100,20);
-    add(200,50);
-  }
-  public static void add(int x, int y){
+  public static int add(int x, int y){
     int ans = x + y;
-    System.out.println(x + "+" + y + "=" + ans);
+    return ans;
+  }
+  public static void main(String[] args){
+    int ans = add(100, 10);
+    System.out.println("100 + 10 = " + ans);
   }
 }
