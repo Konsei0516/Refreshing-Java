40ffdb9ba619e36e47ebf244329f57faf27a5cc7
Konsei0516 <masaya19960516@gmail.com>
2020-07-07 16:40:03 +0900

    引数(数値を渡す場合)

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 public class Main {
   public static void main(String[] args){
-    System.out.println("メソッドを呼び出します");
-    hello("湊");
-    hello("朝香");
-    hello("菅原");
-    System.out.println("メソッドの呼び出しを終了します");
+    add(100,20);
+    add(200,50);
   }
-  public static void hello(String name){
-    System.out.println(name + "さんこんにちは");
+  public static void add(int x, int y){
+    int ans = x + y;
+    System.out.println(x + "+" + y + "=" + ans);
   }
 }
