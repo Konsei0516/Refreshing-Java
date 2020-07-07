49e54e55f4364da209a5e45cfb3a804506551633
Konsei0516 <masaya19960516@gmail.com>
2020-07-07 10:04:39 +0900

    main以外からのメソッドの呼び出し

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("メソッドを呼び出します");
-    hello();
-    System.out.println("メソッドの呼び出しが終わりました");
+  public static void methodA() {
+    System.out.println("methodA");
+    methodB();
   }
-  public static void hello(){
-    System.out.println("湊さんこんにちは");
+  public static void methodB(){
+    System.out.println("methodB");
+  }
+  public static void main(String[] args){
+    methodA();
   }
 }
