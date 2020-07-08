c0a07e160f8cb5b24e930fdb90e395c37e4d5cde
Konsei0516 <masaya19960516@gmail.com>
2020-07-08 13:39:56 +0900

    戻り値に配列を使用

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
-  public static void incArray(int[] array){
-    for (int i = 0; i < array.length; i ++){
-      array[i] ++;
+  public static int[] makeArray(int size){
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.length; i ++){
+      newArray[i] = i;
     }
+    return newArray;
   }
   public static void main(String[] args){
-    int[] array = {1,2,3};
-    incArray(array);
+    int[] array = makeArray(3);
     for (int i : array ) {
       System.out.println(i);
     }
