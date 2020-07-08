1d3424e4aeaba0f35a41b00b048b8a51d8780037
Konsei0516 <masaya19960516@gmail.com>
2020-07-08 13:30:54 +0900

    参照型変数

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main {
-  public static void printArray(int[] array){
-    for (int element : array){
-      System.out.println(element);
+  public static void incArray(int[] array){
+    for (int i = 0; i < array.length; i ++){
+      array[i] ++;
     }
   }
   public static void main(String[] args){
     int[] array = {1,2,3};
-    printArray(array);
+    incArray(array);
+    for (int i : array ) {
+      System.out.println(i);
+    }
   }
 }
