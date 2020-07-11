e0e113c3cd0cdf10778fdb4c9eff4be0dd918103
Konsei0516 <masaya19960516@gmail.com>
2020-07-11 22:47:20 +0900

    全てにimportする

--- Calc.java
+++ Calc.java
@@ -1,10 +1,10 @@
 package calcapp.main;
-import calcapp.logics.CalcLogic;
+import calcapp.logics.*;
 
 public class Calc {
   public static void main(String[] args){
     int a = 10; int b = 2;
-    int total = calcapp.logics.Calclogic.tasu(a,b);
+    int total = Calclogic.tasu(a,b);
     int delta = calcapp.logics.Calclogic.hiku(a,b);
     System.out.println("足すと" + total + "引くと" + delta);
   }
