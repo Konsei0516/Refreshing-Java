4a9cfec8a9adc268339295b61fbf4abb31cbda45
Konsei0516 <masaya19960516@gmail.com>
2020-07-13 21:44:45 +0900

    HPと名前の設定

--- Calclogic.java
+++
@@ -1,9 +0,0 @@
-package calcapp.logics;
-public class Calclogic {
-  public static int tasu(int a, int b){
-    return (a + b);
-  }
-  public static int hiku(int a, int b){
-    return(a - b);
-  }
-}
