46d9796a18c4d9f990b5d2e7d61e32cb44a60c52
Konsei0516 <masaya19960516@gmail.com>
2020-07-14 23:28:26 +0900

    アクションのメソッドを追加

--- Hero.java
+++ Hero.java
@@ -6,4 +6,19 @@ public class Hero {
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した");
   }
+  public void sit(int sec) { 
+    this.hp += sec;
+    System.out.println(this.name + "は、" + sec + "秒座った!");
+    System.out.println("HPが" + sec + "ポインt回復したい");
+  }
+  public void slip() { 
+    this.hp -= 5;
+    System.out.println(this.name + "は転んだ");
+    System.out.println("5のダメージ");
+  }
+  public void run() { 
+    System.out.println(this.name + "は、逃げ出した" );
+    System.out.println("GAMEOVER");
+    System.out.println("最終HPは" + this.hp + "でした");
+  }
 }
