921874156d87e07f18cd66b88012e1a72c55aab5
Konsei0516 <masaya19960516@gmail.com>
2020-07-15 22:51:55 +0900

    mainの設定

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main {
   public static void main(String[] args){
-    int[] heights = {172,149,152,191,155};
-    java.util.Arrays.sort(heights);
-    for (int h : heights){
-      System.out.println(h);
-    }
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました");
+    h.sit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
   }
 }
