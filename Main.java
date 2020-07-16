9225d5c973e052d10c0dcaee8b4d4eb1d851c179
Konsei0516 <masaya19960516@gmail.com>
2020-07-16 22:23:37 +0900

    細かい設定

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
   public static void main(String[] args){
-    Hero h = new Hero();
-    h.name = "ミナト";
-    h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出しました");
-    h.sit(5);
-    h.slip();
-    h.sit(25);
-    h.run();
+    int yusha_hp = 100;
+    int matango1_hp = 50;
+    int matangi2_hp = 48;
+    String yusha_name = "ミナト";
+    int matangi1_level = 10;
+    int matangi2_level = 10;
+    System.out.println(yusha_name + "は5秒座った");
+    yusha_hp += 5;
+    System.out.println("HPがは5ポイント回復した");
   }
 }
