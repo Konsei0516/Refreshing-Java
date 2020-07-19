a336e5e09560d9e0952d60f565d029e874fdfad1
Konsei0516 <masaya19960516@gmail.com>
2020-07-19 21:43:29 +0900

    2つの変数を利用

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
 public class Main {
   public static void main(String[] args){
-    Hero h;
-    h = new Hero();
-    h.hp = 100;
-
-    Matango m1 = new Matango();
-    m1.hp = 50;
-    m1.suffix = 'A';
-    Matango m2 = new Matango();
-    m2.hp = 48;
-    m2.suffix = 'B';
-    
-    h.slip();
-    m1.run();
-    m2.run();
-    h.run();
+    Hero h1;
+    h1 = new Hero();
+    h1.hp = 100;
+    Hero h2;
+    h2 = h1;
+    h2.hp = 200;
+    System.out.println(h1.hp);
   }
 }
