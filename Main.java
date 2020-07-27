e08802abcdaee2d34c25022f426eb8841c0d22f9
Konsei0516 <masaya19960516@gmail.com>
2020-07-27 23:09:27 +0900

    オーバーロー後のクラス利用

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 public class Main {
   public static void main(String[] args){
-    Hero h= new Hero("ミナト");
-    h.hp = 100;
-    Hero h2 = new Hero("アサカ");
+    Hero h1 = new Hero("ミナト");
+    System.out.println(h1.name);
+    h1.hp = 100;
+    Hero h2 = new Hero();
+    System.out.println(h2.name);
     h2.hp = 200;
     Wizard w = new Wizard();
     w.name = "スガワラ";
     w.hp = 50;
-    w.heal(h);
+    w.heal(h1);
     w.heal(h2);
     w.heal(h2);
-    System.out.println(h.hp);
-    System.out.println(h.name);
+    System.out.println(h1.hp);
+    System.out.println(h1.name);
   }
 }
