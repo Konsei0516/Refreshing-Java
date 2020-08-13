05a834d74de7787b3e0426bb5d2d39abe5f811e4
Konsei0516 <masaya19960516@gmail.com>
2020-08-13 23:11:27 +0900

    インスタンス化

--- Wizard.java
+++ Wizard.java
@@ -1,8 +1,14 @@
-public class Wizard {
-  String name;
-  int hp;
-  public void heal (Hero h){
-    h.hp += 10;
-    System.out.println(h.name + "のHPを１０回復した");
+public class Wizard extends Character{
+  int mp;
+  public void attack (Matango m){
+    System.out.println(this.name + "の攻撃");
+    System.out.println("敵に3ポイントのダメージ");
+    m.hp -= 3;
+  }
+  public void fireball(Matango m){
+    System.out.println(this.name + "は火の玉を放った");
+    System.out.println("敵に20ポイントのダメージ");
+    m.hp -= 20;
+    this.mp -= 5;
   }
 }
