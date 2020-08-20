0ec9b52e5d28d3f56e75c90444848bac7943efdf
Konsei0516 <masaya19960516@gmail.com>
2020-08-20 22:56:45 +0900

    王様の設定
    nameフィールドにアクセス部分をgetNameで呼び出す。

--- Hero.java
+++ Hero.java
@@ -38,4 +38,8 @@ public class Hero{
     System.out.println(this.name + "は死んでしまった");
     System.out.println("GAME OVER");
   }
+
+  public String getName(){
+    return this.name;
+  }
 }
---
+++ King.java
@@ -0,0 +1,5 @@
+public class King {
+  void talk(Hero h){
+    System.out.println("王様：ようこそ我が国へ,勇者" + h.getName());
+  }
+}
