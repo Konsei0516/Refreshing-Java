dcc7d9a452070e20a20fcc74604ac51617a2427b
Konsei0516 <masaya19960516@gmail.com>
2020-08-23 18:20:28 +0900

    多態性の利用

--- Main.java
+++ Main.java
@@ -1,20 +1,7 @@
 public class Main {
   public static void main(String[] args){
-    SuperHero sh = new SuperHero();
-    Weapon w = new Weapon();
-    Slime s = new Slime(); Monster m = new Slime();
-    s.run(); m.run();
-    sh.run();
-    Hero h1 = new Hero("ミナト");
-    System.out.println(h1.name);
-    h1.hp = 100;
-    Hero h2 = new Hero();
-    System.out.println(h2.name);
-    h2.hp = 200;
-    System.out.println(h1.hp);
-    System.out.println(h1.name);
-    Empty e = Empty();
-    String s = e.toString();
-    System.out.println(s);
+    Object o1 = new Empty();
+    Object o2 = new Hero();
+    Object o3 = "こんにちは";
   }
 }
