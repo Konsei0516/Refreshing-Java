4252654733769b6c63eb73103c1a57b883a00aa0
Konsei0516 <masaya19960516@gmail.com>
2020-08-25 22:03:07 +0900

    equals
    equalsでの等価判定

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main {
   public static void main(String[] args){
-    Hero h = new Hero();
-    h.name = "港";
-    h.hp = 100;
-    System.out.println(h.toString());
+    Hero h1 = new Hero();
+    h1.name = "港";
+    h1.hp = 100;
+    Hero h2 = new Hero();
+    h2.name = "ミナト";
+    h2.hp = 100;
+    if(h1.equals(h2) == true){
+      System.out.println("同じ内容です");
+    } else {
+      System.out.println("違う内容です");
+    }
   }
 }
