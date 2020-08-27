efd08d1ac9bc500e3f2229eb69dff8ce367c704c
Konsei0516 <masaya19960516@gmail.com>
2020-08-27 21:49:07 +0900

    Update Main.java
    インスタンスから静的フィールドの利用

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
 public class Main {
-  public static void main(String[] args){
+  public static void main(final String[] args) {
     Hero h1 = new Hero();
-    h1.name = "港";
-    h1.hp = 100;
     Hero h2 = new Hero();
-    h2.name = "ミナト";
-    h2.hp = 100;
-    if(h1.equals(h2) == true){
-      System.out.println("同じ内容です");
-    } else {
-      System.out.println("違う内容です");
-    }
+
+    Hero.money = 100;
     System.out.println(h1.hp);
     System.out.println(Hero.money);
+    System.out.println(h1.money);
+    h1.money = 300;
+    System.out.println(h2.money);
   }
 }
