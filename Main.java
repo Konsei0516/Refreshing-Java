4dfea7bb37b7fd2fad5d5ee8971f930d7b6d3318
Konsei0516 <masaya19960516@gmail.com>
2020-08-29 22:23:42 +0900

    切り出しメソッド

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
 public class Main {
   public static void main(final String[] args) {
-    String s1 = "Java and JavaScript";
-    if(s1.contains("Java")){
-      System.out.println("文字列s1はjavaを含んでいます");
-    }
-    if(s1.endsWith("Java")){
-      System.out.println("文字列s1はjavaが末尾にあります");
-    }
-    System.out.println("文字列で最初にJavaが登場するのは" + s1.indexOf("Java"));
-    System.out.println("文字列で最後にJavaが登場するのは" + s1.lastIndexOf("Java"));
+    String s1 = "Java programming";
+    System.out.println("文字列s1の４文字目以降は" + s1.substring(3));
+    System.out.println("文字列s1の４~8文字目以降は" + s1.substring(3,8));
   }
 }
