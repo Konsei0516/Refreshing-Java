8417c33741a8dc4fe113fcc0f32d01fc53612c68
Konsei0516 <masaya19960516@gmail.com>
2020-08-30 17:05:22 +0900

    StringBuilderを利用
    appendメソッドを読んで文字列を追加

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(final String[] args) {
-    String s1 = "Java programming";
-    System.out.println("文字列s1の４文字目以降は" + s1.substring(3));
-    System.out.println("文字列s1の４~8文字目以降は" + s1.substring(3,8));
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < 1000; i++){
+      sb.append("Java");
+    }
+    String s = sb.toString();
   }
 }
