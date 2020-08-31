d82dc970c2f1b6ace24dccd9fae77193226deabd
Konsei0516 <masaya19960516@gmail.com>
2020-08-31 23:16:52 +0900

    replaceAllメソッド
    文字列を置換する

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     String s = "abc,def:ghi";
-    String[] words = s.split("[,:]");
-    for(String w : words){
-      System.out.println(w + "->");
-    }
+    String w = s.replaceAll("[beh]","X");
+    System.out.println(w);
   }
 }
