6e8ff14fd5468adddd0051b1dcd4645eaa2d84a1
Konsei0516 <masaya19960516@gmail.com>
2020-09-02 23:08:22 +0900

    現在日時の表示

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    long end = System.currentTimeMillis();
-    System.out.println("処理にかかった時間は" + (end - start) + "ミリ秒でした");
+    Date now = new Date();
+    System.out.println(now);
+    System.out.println(now.getTime());
+    Date past = new Date(16007054258827L);
+    System.out.println(past);
   }
 }
