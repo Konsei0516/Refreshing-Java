ac41cb3dcc1e938e239d91f3e9a37fb0fe3072eb
Konsei0516 <masaya19960516@gmail.com>
2020-09-03 23:16:59 +0900

    int値とDate型

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
   public static void main(String[] args) {
+    Carendar c = Calendar.getInstance();
+    c.set(2019,8,22,1,23,45);
+    c.set(Calendar.MONTH,9);
+    Data d = c.getTime();
+    System.out.println(d);
     Date now = new Date();
-    System.out.println(now);
-    System.out.println(now.getTime());
-    Date past = new Date(16007054258827L);
-    System.out.println(past);
+    c.setTime(now);
+    int y = c.get(Calendar.YEAR);
+    System.out.println("今年は" + y + "年です");
   }
 }
