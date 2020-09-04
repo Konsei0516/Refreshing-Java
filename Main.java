a36a2d9cb211e5602202b326ceabbdfc08ec2746
Konsei0516 <masaya19960516@gmail.com>
2020-09-04 21:52:31 +0900

    String型とDate型の相関

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-  public static void main(String[] args) {
-    Carendar c = Calendar.getInstance();
-    c.set(2019,8,22,1,23,45);
-    c.set(Calendar.MONTH,9);
-    Data d = c.getTime();
+  public static void main(String[] args) throws Exception{
+    SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Date d = f.parse("2020/09/22 01:23:45");
     System.out.println(d);
     Date now = new Date();
-    c.setTime(now);
-    int y = c.get(Calendar.YEAR);
-    System.out.println("今年は" + y + "年です");
+    String s = f.format(now);
+    System.out.println("現在" + s + "です");
   }
 }
