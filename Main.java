9c416f67a7fc2b149b7637d1ec7766fce4b244b4
Konsei0516 <masaya19960516@gmail.com>
2020-09-05 23:42:12 +0900

    ZonedDateTimeの利用

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.*;
 
 public class Main {
   public static void main(String[] args) throws Exception{
-    SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    Date d = f.parse("2020/09/22 01:23:45");
-    System.out.println(d);
-    Date now = new Date();
-    String s = f.format(now);
-    System.out.println("現在" + s + "です");
+    Instant i1 = Instant.now();
+    Instant i2 = Instant.ofEpochMilli(1600705425827L);
+    long 1 = i2.toEpochMilli();
+
+    //ZoneDateTimeの生成
+    ZonedDateTime z1 = ZonedDateTime.now();
+    ZonedDateTime z2 = ZonedDateTime.of(2020,1,2,3,4,5,6, ZoneId.of("Asia/Tokyo"));
+
+    Instant i3 = z2.toInstant();
+    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
+
+    //ZoneDateTimeの利用
+    System.out.println("東京" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+    System.out.println("ロンドン" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
+    if(z2.isEqual(z3)){
+      System.out.println("これらは同じ瞬間をさしています");
+    }
   }
 }
