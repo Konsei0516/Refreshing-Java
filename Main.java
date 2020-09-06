24c9d09d71f56470ad71bdbb97ac10345a28efb2
Konsei0516 <masaya19960516@gmail.com>
2020-09-06 23:22:30 +0900

    LocalDateTimeの利用

--- Main.java
+++ Main.java
@@ -2,22 +2,11 @@ import java.time.*;
 
 public class Main {
   public static void main(String[] args) throws Exception{
-    Instant i1 = Instant.now();
-    Instant i2 = Instant.ofEpochMilli(1600705425827L);
-    long 1 = i2.toEpochMilli();
-
     //ZoneDateTimeの生成
-    ZonedDateTime z1 = ZonedDateTime.now();
-    ZonedDateTime z2 = ZonedDateTime.of(2020,1,2,3,4,5,6, ZoneId.of("Asia/Tokyo"));
+    LocalDateTime l1 = LocalDateTime.now();
+    LocalDateTime l2 = LocalDateTime.of(2020,1,1,9,5,0,0);
 
-    Instant i3 = z2.toInstant();
-    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-
-    //ZoneDateTimeの利用
-    System.out.println("東京" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
-    System.out.println("ロンドン" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
-    if(z2.isEqual(z3)){
-      System.out.println("これらは同じ瞬間をさしています");
-    }
+    ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+    LocalDateTime l3 = z1.toLocalDateTime();
   }
 }
