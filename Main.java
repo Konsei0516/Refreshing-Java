8ce075b58a6fb7d01efb79747b1d62825602c44e
Konsei0516 <masaya19960516@gmail.com>
2020-09-08 23:19:15 +0900

    ArrayListの格納
    WrraperクラスをArrayListへ格納する

--- Main.java
+++ Main.java
@@ -1,20 +1,13 @@
-import java.time.*;
-import java.time.format.*;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) throws Exception{
-    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-    LocalDate ldate = LocalDate.parse("2020/09/22",fmt);
-
-    //1000日後を計算
-    LocalDate ldatep = ldate.plusDays(1000);
-    String str = ldatep.format(fmt);
-    System.out.println("1000日後は" + str);
-
-    //現在との比較
-    LocalDate now = LocalDate.now();
-    if(now.isAfter(ldatep)){
-      System.out.println("1000日後は過去日付です");
+    ArrayList<Integer>points = new ArrayList<Integer>();
+    points.add(10);
+    points.add(80);
+    points.add(75);
+    for(int i : points){
+      System.out.println(i);
     }
   }
 }
