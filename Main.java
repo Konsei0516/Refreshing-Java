236d53ddec03a1b1341e595751fbc08cd8b3c842
Konsei0516 <masaya19960516@gmail.com>
2020-09-09 23:44:10 +0900

    イテレータ
    イテレータを使用したArrayListの繰り返し

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws Exception{
-    ArrayList<Integer>points = new ArrayList<Integer>();
-    points.add(10);
-    points.add(80);
-    points.add(75);
-    for(int i : points){
-      System.out.println(i);
+    ArrayList<String>names = new ArrayList<String>();
+    names.add("港");
+    names.add("朝日");
+    names.add("菅原");
+    Iterator<String>it = names.iterator();
+    while(it.hasNext()) {
+      String e = it.next();
+      System.out.println(e);
     }
   }
 }
