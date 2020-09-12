066ec0f01095870b3f46875d2263d6f7c8600a6c
Konsei0516 <masaya19960516@gmail.com>
2020-09-12 23:18:27 +0900

    HashMapの利用

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws Exception{
-    Set<String> colors = new HashSet<String>();
-    colors.add("赤");
-    colors.add("青");
-    colors.add("黄");
-    colors.add("赤");
-    for(String s : colors){
-      System.out.println(s + "⇨");
-    }
+    Map<String,Integer> prefs = new HashMap<String,Integer>();
+    prefs.put("京都",255);
+    prefs.put("東京",255);
+    prefs.put("熊本",255);
+    int tokyo = prefs.get("東京");
+    System.out.println("東京との人口は" + tokyo);
+    prefs.remove("京都");
+    prefs.put("熊本",182);
+    int kumamoto = prefs.get("熊本");
+    System.out.println("熊本の人口は" + kumamoto);
   }
 }
