3db74b346be5f2349cb4bf92f5c781b6a60f81d1
Konsei0516 <masaya19960516@gmail.com>
2020-09-13 22:53:10 +0900

    マップの取り出し
    マップに格納されたものを一つずつ取り出す方法

--- Main.java
+++ Main.java
@@ -4,13 +4,11 @@ public class Main {
   public static void main(String[] args) throws Exception{
     Map<String,Integer> prefs = new HashMap<String,Integer>();
     prefs.put("京都",255);
-    prefs.put("東京",255);
-    prefs.put("熊本",255);
-    int tokyo = prefs.get("東京");
-    System.out.println("東京との人口は" + tokyo);
-    prefs.remove("京都");
+    prefs.put("東京",1261);
     prefs.put("熊本",182);
-    int kumamoto = prefs.get("熊本");
-    System.out.println("熊本の人口は" + kumamoto);
+    for(String key : prefs.keySet()){
+      int value = prefs.get(key);
+      System.out.println(key + "の人口は" + value);
+    }
   }
 }
