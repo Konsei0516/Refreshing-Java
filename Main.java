f07b9f63fb412351ad843b08f575d807b8a94e99
Konsei0516 <masaya19960516@gmail.com>
2020-09-15 22:42:34 +0900

    例外処理
    例外の補足

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
-import java.util.*;
-
-class Hero {
-  public String name;
-}
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception{
-    Hero h = new Hero();
-    h.name = "港";
-    List<Hero>list = new ArrayList<Hero>();
-    list.add(h);
-    h.name = "菅原";
-    System.out.println(list.get(0).name);
+  public static void main(String[] args){
+    try{
+      FileWriter fw = new FileWriter("data.txt");
+      fw.write("hello");
+      fw.close();
+    } catch(IOException e) {
+      System.out.println("エラーが発生しました、中断します");
+    }
   }
 }
