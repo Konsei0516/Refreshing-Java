2754706545dce452672cf517e7ad11888aee1686
Konsei0516 <masaya19960516@gmail.com>
2020-09-20 17:07:00 +0900

    WEBページの取得

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
-import java.io.*;
+import java.io.InputStream;
+import java.net.URL;
 
 public class Main {
   public static void main(String[] args){
-    FileReader fr = new FileReader("data.txt");
-    int input = fr.read();
-    while (input != -1){
-      System.out.println((char)input);
-      input = fr.read();
+    URL u = new URL("https://book.ompress.co.jp/");
+    InputStream is = u.openStream();
+    int i = is.read();
+    while (i != -1){
+      char c = (char)i;
+      System.out.println(c);
+      i = is.read();
     }
-    fr.close();
   }
 }
