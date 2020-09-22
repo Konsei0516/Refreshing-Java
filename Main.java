feeaeeee95c25e26a9045ea068d52af84c43d1e4
Konsei0516 <masaya19960516@gmail.com>
2020-09-22 21:16:58 +0900

    GUIプログラム

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
-import java.sql.*;
+import java.awt.FlowLayout;
+import javax.swing.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception{
-    Class.forName("org.h2.Driver");
-    String dburl = "jdbc:h2:~/test";
-    String sql = "INSERT INTO EMPLOYEES(name) VALUES('aoki')";
-    Connection conn = DriverManager.getConnection(dburl);
-    conn.createStatement().executeUpdate(sql);
-    conn.close();
+  public static void main(String[] args){
+    JFrame frame = new JFrame("初めてのGUI");
+    JLabel label = new JLabel("Hello");
+    JButton button = new JButton("押してね");
+    frame.getContentPane().setLayout(new FlowLayout());
+    frame.getContentPane().add(label);
+    frame.getContentPane().add(button);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300,100);
+    frame.setVisible(true);
   }
 }
