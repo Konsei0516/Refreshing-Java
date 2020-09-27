b2e44be7d156da00c0adcff8fe8ffdec87e2051c
Konsei0516 <masaya19960516@gmail.com>
2020-09-27 23:31:34 +0900

    データの表示

--- Basic/Main.java
+++ Basic/Main.java
@@ -7,5 +7,9 @@ class UserClassCode{
     student1.name = "鈴木";
     student1.gender = '男';
     student1.score = 45;
+
+    System.out.println("名前:" + student1.name);
+    System.out.println("性別:" + student1.gender);
+    System.out.println("点数:" + student1.score);
   }
 }
