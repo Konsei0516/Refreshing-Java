fead232af2e2a1a59b31d40e25f820be12e62cc4
Konsei0516 <masaya19960516@gmail.com>
2020-09-27 23:26:38 +0900

    フィールドの値を代入

---
+++ Basic/Main.java
@@ -0,0 +1,11 @@
+package Basic;
+
+class UserClassCode{
+  public static void main(String[] args) {
+    Student student1 = new Student();
+
+    student1.name = "鈴木";
+    student1.gender = '男';
+    student1.score = 45;
+  }
+}
