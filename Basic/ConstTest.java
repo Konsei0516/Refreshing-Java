0afcc0d74a8f201344b960dfaef28979a88b8e17
Konsei0516 <masaya19960516@gmail.com>
2020-10-01 22:52:07 +0900

    コンストラクタの定義

---
+++ Basic/ConstTest.java
@@ -0,0 +1,11 @@
+package Basic;
+
+class ConstTest {
+  public static void main(String[] args){
+    Employee shain1 = new Employee(1, "佐藤としこ");
+    Employee shain2 = new Employee(2, "田中たいち");
+
+    shain1.dispMethod();
+    shain2.dispMethod();
+  }
+}
