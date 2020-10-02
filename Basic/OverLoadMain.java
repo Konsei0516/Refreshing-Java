bc5434e1c80bd5994e91595c00cb9de7552bc975
Konsei0516 <masaya19960516@gmail.com>
2020-10-02 23:06:38 +0900

    オーバーロード
    引数の数を変えることで同じメソッドを定義できました

---
+++ Basic/OverLoadMain.java
@@ -0,0 +1,9 @@
+package Basic;
+
+public class OverLoadMain {
+  public static void main(String[] args){
+    Shopping shop = new Shopping();
+    shop.buy(300);
+    shop.buy(300,5);
+  }
+}
--- Basic/Shopping.java
+++ Basic/Shopping.java
@@ -1,5 +1,12 @@
 package Basic;
 
 public class Shopping {
-  
+  //引数1
+  public void buy(int price){
+    System.out.println("商品は" + price + "円です");
+  }
+  //引数2
+  public void buy(int price, int quantity){
+    System.out.println("商品" + quantity + "つの合計は" + price * quantity + "円です");
+  }
 }
