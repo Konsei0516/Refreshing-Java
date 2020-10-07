6cc780b4e1d6577170c1dd3dfb4a9930c8c611c5
Konsei0516 <masaya19960516@gmail.com>
2020-10-07 22:56:47 +0900

    インターフェース
    2つのクラスをインターフェースを実装することでButton型変数として扱う

---
+++ Basic/Close.java
@@ -0,0 +1,7 @@
+package Basic;
+
+public class Close implements Button {
+  public void push(){
+    System.out.println("閉じるボタンを押しました");
+  }
+}
---
+++ Basic/InterfaceMain.java
@@ -0,0 +1,9 @@
+package Basic;
+
+public class InterfaceMain {
+  public static void main(String[] args){
+    System.out.println(Button.size);
+    Button btn = new Save();
+    btn.push();
+  }
+}
